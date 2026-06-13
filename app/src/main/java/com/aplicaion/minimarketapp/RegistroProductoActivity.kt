2dbbc99dc17283a46e2ff3c2f500e7a048a055ef
lyric-pasto <jhoseph0.26@gmail.com>
2026-06-12 21:29:28 -0500

    Scanner
    Implementacion de scanner

---
+++ app/src/main/java/com/aplicaion/minimarketapp/RegistroProductoActivity.kt
@@ -0,0 +1,118 @@
+package com.aplicaion.minimarketapp
+
+import android.Manifest
+import android.os.Bundle
+import android.util.Size
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.OptIn
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.core.resolutionselector.ResolutionStrategy
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.aplicaion.minimarketapp.databinding.ActivityRegistroProductoBinding
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class RegistroProductoActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistroProductoBinding
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var barcodeScanner: BarcodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistroProductoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        barcodeScanner = BarcodeScanning.getClient()
+
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+                if (isGranted) {
+                    startCamera()
+                } else {
+                    binding.resultTextView.text = "Permisos de cámara necesarios"
+                }
+            }
+
+        requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        val screenSize = Size(1280, 720)
+        val resolutionSelector = ResolutionSelector.Builder().setResolutionStrategy(
+            ResolutionStrategy(screenSize, ResolutionStrategy.FALLBACK_RULE_NONE)
+        ).build()
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().setResolutionSelector(resolutionSelector)
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.previewView.surfaceProvider)
+                }
+            val imageAnalyzer = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, { imageProxy ->
+                        processImageProxy(imageProxy)
+                    })
+                }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    @OptIn(ExperimentalGetImage::class)
+    private fun processImageProxy(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            barcodeScanner.process(image)
+                .addOnSuccessListener { barcodes ->
+                    for (barcode in barcodes){
+                        handleBarcode(barcode)
+                    }
+                }
+                .addOnFailureListener{
+                    binding.resultTextView.text = "Fallo al escanear el codigo"
+                }
+                .addOnCompleteListener {
+                    imageProxy.close()
+                }
+
+        }
+    }
+
+    private fun handleBarcode(barcode: Barcode) {
+        val txt = barcode.url?.url ?: barcode.displayValue
+
+        binding.resultTextView.text =
+            txt ?: "No se detectó código"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
+    }
+}
